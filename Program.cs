989589e74172bedb66744983ce18dc76ecf715f2
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-20 15:33:09 +0530

    Ganesh adding program file for uc13

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LambdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1Name name = new UC1Name();
+
+
+            Console.WriteLine("*************name********");
+
+            Console.WriteLine("Enter First Name");
+            string FirstName=Console.ReadLine();
+            string opt=name.FirstnameCheckusingLambda(FirstName);
+            if(opt=="FirstName is valid")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(opt);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(opt);
+                Console.ResetColor();
+            }
+
+
+            }
+        }
+    }
